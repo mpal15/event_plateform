a247478af9f7332ef13f183565cecf4b77cf4b3a
Mohit pal <mohitpal9513@gmail.com>
2024-02-01 11:17:10 +0530

    update the schema structure

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,15 +1,14 @@
-import { Schema ,model ,models, Document} from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface ICategory extends Document {
-    _id: String;
-    name:String;
+  _id: string;
+  name: string;
 }
 
 const CategorySchema = new Schema({
-    name: { type: String, required: true,
-    unique: true},
+  name: { type: String, required: true, unique: true },
 })
 
-const Category = models.Category || model('Category' , CategorySchema);
+const Category = models.Category || model('Category', CategorySchema);
 
 export default Category;
